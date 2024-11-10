b40cf55f5b96758adc95f81c9030fbedc7332ff2
Yu <67401786+knz-yu-github@users.noreply.github.com>
2024-11-10 13:30:39 +0900

    Delete イントラBox対応 directory Deleted

--- イントラBox対応/FileSelect.ps1
+++
@@ -1,5 +0,0 @@
-$targetdir = "C:\"
-#                          再帰  エラー無視(ファイル権限、破損などすべて)               (Where-Object)拡張子絞り込み         (Select-Object)絶対パスを選択                  txtファイルへ出力
-Get-ChildItem $targetdir -Recurse -ErrorAction SilentlyContinue | Where-Object { $_.Extension -in ".png", ".jpeg" } | Select-Object -ExpandProperty FullName | Out-File ".\output.txt"
-
-#$_ は、PowerShell の パイプライン（Pipeline） で使用される特殊な変数です。$_ は、パイプライン内の現在のオブジェクトを指します。
